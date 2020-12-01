ad85e6d28ed0f5ff18021c1c22045cab7c13760f
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2020-12-01 21:33:29 +0100

    Day 1.

--- src/main/kotlin/day01/ReportRepair.kt
+++ src/main/kotlin/day01/ReportRepair.kt
@@ -6,9 +6,11 @@ class ReportRepair(private val entries: IntArray) {
     private val sum = 2020
 
     fun part1(): Int {
-        val pairs = (0 until size - 1).flatMap { i ->
-            (i + 1 until size).map { j -> Pair(i, j) }
-        }
+        val pairs = (0 until size - 1)
+            .asSequence()
+            .flatMap { i ->
+                (i + 1 until size).map { j -> Pair(i, j) }
+            }
         return pairs
             .filter { (i, j) -> entries[i] + entries[j] == sum }
             .map { (i, j) -> entries[i] * entries[j] }
@@ -16,11 +18,13 @@ class ReportRepair(private val entries: IntArray) {
     }
 
     fun part2(): Int {
-        val triples = (0 until size - 2).flatMap { i ->
-            (i + 1 until size - 1).flatMap { j ->
-                (j + 1 until size).map { k -> Triple(i, j, k) }
+        val triples = (0 until size - 2)
+            .asSequence()
+            .flatMap { i ->
+                (i + 1 until size - 1).flatMap { j ->
+                    (j + 1 until size).map { k -> Triple(i, j, k) }
+                }
             }
-        }
         return triples
             .filter { (i, j, k) -> entries[i] + entries[j] + entries[k] == sum }
             .map { (i, j, k) -> entries[i] * entries[j] * entries[k] }
