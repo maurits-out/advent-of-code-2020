4da897d047c9f503f0656ac41cd13f1653fc41ff
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2020-12-06 17:10:17 +0100

    Some improvements.

--- src/main/kotlin/day04/PassportProcessing.kt
+++ src/main/kotlin/day04/PassportProcessing.kt
@@ -17,13 +17,13 @@ class PassportProcessing(private val input: String) {
     private val passportIdRegEx = "\\d{9}".toRegex()
 
     private val validators = mapOf<String, (String) -> Boolean>(
-        Pair("byr", { isValidYear(it, 1920, 2020) }),
-        Pair("iyr", { isValidYear(it, 2010, 2020) }),
-        Pair("eyr", { isValidYear(it, 2020, 2030) }),
-        Pair("hgt", { it.matches(heightRegex) && (isValidHeightInCm(it) || isValidHeightInIn(it)) }),
-        Pair("hcl", { it.matches(hairColorRegex) }),
-        Pair("ecl", { eyeColors.contains(it) }),
-        Pair("pid", { it.matches(passportIdRegEx) })
+        "byr" to { isValidYear(it, 1920, 2020) },
+        "iyr" to { isValidYear(it, 2010, 2020) },
+        "eyr" to { isValidYear(it, 2020, 2030) },
+        "hgt" to { it.matches(heightRegex) && (isValidHeightInCm(it) || isValidHeightInIn(it)) },
+        "hcl" to { it.matches(hairColorRegex) },
+        "ecl" to { it in eyeColors },
+        "pid" to { it.matches(passportIdRegEx) }
     )
 
     private fun containsRequiredFields(passport: Passport): Boolean =
@@ -48,27 +48,12 @@ class PassportProcessing(private val input: String) {
     private fun isValidYear(value: String, min: Int, max: Int) =
         value.matches(yearRegex) && value.toInt() in min..max
 
-    private fun parsePassports(): Set<Passport> {
-        val passports = hashSetOf<Passport>()
-        val currentPassport = hashSetOf<Pair<String, String>>()
-        for (line in input.lines()) {
-            if (line.isEmpty()) {
-                passports.add(currentPassport.toMap())
-                currentPassport.clear()
-            } else {
-                val fields = extractFields(line)
-                currentPassport.addAll(fields)
-            }
-        }
-        return passports
-    }
+    private fun parsePassports(): List<Passport> = input.split("\n\n")
+            .map { it.replace('\n', ' ') }
+            .map { extractFields(it) }
 
-    private fun extractFields(line: String): Set<Pair<String, String>> {
-        val fields = hashSetOf<Pair<String, String>>()
-        val parts = line.split(':', ' ')
-        for (i in parts.indices step 2) {
-            fields.add(Pair(parts[i], parts[i + 1]))
+    private fun extractFields(line: String): Map<String, String> = line.split(' ')
+        .associate {
+            it.split(':').let { (field, value) -> field to value }
         }
-        return fields
-    }
 }
