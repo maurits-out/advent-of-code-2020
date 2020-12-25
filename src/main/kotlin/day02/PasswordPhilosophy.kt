486d298c8a2277652217b7e6c7f069195964d9ce
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2020-12-25 15:31:33 +0100

    Refactoring.

--- src/main/kotlin/day02/PasswordPhilosophy.kt
+++ src/main/kotlin/day02/PasswordPhilosophy.kt
@@ -2,21 +2,19 @@ package day02
 
 class PasswordPhilosophy(private val input: List<String>) {
 
-    fun part1(): Int = input.count { isValidPasswordPart1(it) }
+    fun part1() = input.count { isValidPasswordPart1(it) }
 
-    fun part2(): Int = input.count { isValidPasswordPart2(it) }
+    fun part2() = input.count { isValidPasswordPart2(it) }
 
-    private fun isValidPasswordPart1(line: String): Boolean {
-        return isValidPassword(line) { password, char, min, max ->
+    private fun isValidPasswordPart1(line: String) =
+        isValidPassword(line) { password, char, min, max ->
             password.count { it == char } in min..max
         }
-    }
 
-    private fun isValidPasswordPart2(line: String): Boolean {
-        return isValidPassword(line) { password, char, pos1, pos2 ->
+    private fun isValidPasswordPart2(line: String) =
+        isValidPassword(line) { password, char, pos1, pos2 ->
             (password[pos1 - 1] == char) xor (password[pos2 - 1] == char)
         }
-    }
 
     private fun isValidPassword(line: String, predicate: (CharArray, Char, Int, Int) -> Boolean): Boolean {
         val components = line.split(' ', '-', ':').toTypedArray()
