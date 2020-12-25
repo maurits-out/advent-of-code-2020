5607fb2783044c6dff957a4e565c031b16ba3f30
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2020-12-25 21:11:08 +0100

    Refactoring.

--- src/main/kotlin/day04/PassportProcessing.kt
+++ src/main/kotlin/day04/PassportProcessing.kt
@@ -4,17 +4,17 @@ typealias Passport = Map<String, String>
 
 class PassportProcessing(private val input: String) {
 
-    fun part1(): Int = parsePassports().count { containsRequiredFields(it) }
+    fun part1() = parsePassports().count { containsRequiredFields(it) }
 
-    fun part2(): Int = parsePassports().count { containsRequiredFields(it) && containsValidFields(it) }
+    fun part2() = parsePassports().count { containsRequiredFields(it) && containsValidFields(it) }
 
     private val requiredFields = setOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
 
-    private val yearRegex = "\\d{4}".toRegex()
-    private val heightRegex = "\\d+(?:cm|in)".toRegex()
-    private val hairColorRegex = "#[0-9a-f]{6}".toRegex()
+    private val yearRegex = Regex("""\d{4}""")
+    private val heightRegex = Regex("""\d+(?:cm|in)""")
+    private val hairColorRegex = Regex("""#[0-9a-f]{6}""")
+    private val passportIdRegEx = Regex("""\d{9}""")
     private val eyeColors = setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
-    private val passportIdRegEx = "\\d{9}".toRegex()
 
     private val validators = mapOf<String, (String) -> Boolean>(
         "byr" to { isValidYear(it, 1920, 2020) },
@@ -26,19 +26,16 @@ class PassportProcessing(private val input: String) {
         "pid" to { it.matches(passportIdRegEx) }
     )
 
-    private fun containsRequiredFields(passport: Passport): Boolean =
-        passport.keys.containsAll(requiredFields)
+    private fun containsRequiredFields(passport: Passport) = passport.keys.containsAll(requiredFields)
 
-    private fun containsValidFields(passport: Passport): Boolean =
-        validators
-            .filterNot { entry ->
-                val field = entry.key
-                val validator = entry.value
-                validator(passport.getValue(field))
-            }
-            .isEmpty()
+    private fun containsValidFields(passport: Passport) =
+        validators.filterNot { entry ->
+            val field = entry.key
+            val validator = entry.value
+            validator(passport.getValue(field))
+        }.isEmpty()
 
-    private fun isValidHeightInIn(value: String): Boolean = isValidHeightRange(value, "in", 59, 76)
+    private fun isValidHeightInIn(value: String) = isValidHeightRange(value, "in", 59, 76)
 
     private fun isValidHeightInCm(value: String) = isValidHeightRange(value, "cm", 150, 193)
 
@@ -48,12 +45,13 @@ class PassportProcessing(private val input: String) {
     private fun isValidYear(value: String, min: Int, max: Int) =
         value.matches(yearRegex) && value.toInt() in min..max
 
-    private fun parsePassports(): List<Passport> = input.split("\n\n")
-            .map { it.replace('\n', ' ') }
-            .map { extractFields(it) }
+    private fun parsePassports(): List<Passport> =
+        input.split("\n\n")
+            .map { extractFields(it.replace('\n', ' ')) }
 
-    private fun extractFields(line: String): Map<String, String> = line.split(' ')
-        .associate {
-            it.split(':').let { (field, value) -> field to value }
-        }
+    private fun extractFields(line: String) =
+        line.split(' ')
+            .associate {
+                it.split(':').let { (field, value) -> field to value }
+            }
 }
